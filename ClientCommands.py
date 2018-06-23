9a4d9a466db48e8cb4843177b31fc31d07474f12
stell013 <samiratellezricardo16@gmail.com>
2018-06-23 14:50:44 -0400

    SEARCH DEPARTURE added

--- ClientCommands.py
+++ ClientCommands.py
@@ -49,12 +49,41 @@ def travelFrom(searchCommand):
 
                  if flight.split("-")[1] == dest: #if dest is on dictionary
                     print(origin)
+                  # print(flight) In case we need the entire route
 
      else:
          print ("INVALID COMMAND. PLEASE TRY AGAIN, DID YOU MEAN 'SEARCH dest' ? ")
 
 
 
-print("\nThe departures available will be displyed\n")
-dests = travelFrom('SEARCH MEX')
+#print("\nThe departures available will be displyed\n")
+#dests = travelFrom('SEARCH MEX')
+
+
+#SEARCHDEPARTURE dep method
+
+def travelTo(searchCommand):
+    content = searchCommand.split(" ")
+    command = content[0]
+    dep = content[1]     #departure name
+
+    # try to validate it for capitalize letters
+    if command == 'SEARCHDEPARTURE':
+
+        for flight in dic.keys():  # return all the complete trips
+
+            origin = flight.split("-")[0]  # get the departure place
+            dest = flight.split("-")[1]
+
+            if origin == dep:  # if dest is on dictionary
+                # print(dest)
+                 print(flight) #In case we need the entire route
+
+    else:
+        print ("INVALID COMMAND. PLEASE TRY AGAIN, DID YOU MEAN 'SEARCHDEPARTURE dep' ? ")
+
+
+print("\nThe destinations available will be displyed\n")
+dests = travelTo('SEARCHDEPARTURE HAV')
+
 
