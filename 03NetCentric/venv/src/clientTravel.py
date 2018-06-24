ba36cfe9e67949dbfb667282e84e2b11123b8f05
stell013 <samiratellezricardo16@gmail.com>
2018-06-24 16:26:53 -0400

    while loop added to client script

--- 03NetCentric/venv/src/clientTravel.py
+++ 03NetCentric/venv/src/clientTravel.py
@@ -3,22 +3,27 @@ serverName = 'localhost'
 #import pickle
 import json
 
-serverPort = 10000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-print("Welcome to the TravelAgency")
+"""def startConn():
+	serverPort = 10000
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
+	print("Welcome to the TravelAgency")"""
+
+while True:
+	serverPort = 10000
+	clientSocket = socket(AF_INET, SOCK_STREAM)
+	clientSocket.connect((serverName, serverPort))
+	
+
+	sentence = input('Tell us what do you need : ')
+	clientSocket.send(sentence.encode())
+
+	modifiedSentence = clientSocket.recv(1024)
+	decodeSent = modifiedSentence.decode()
+	myData = json.loads(decodeSent)
+
+	for up in myData:
+  		 print(up)
 
 
-sentence = input('Tell us what do you need : ')
-clientSocket.send(sentence.encode())
-
-modifiedSentence = clientSocket.recv(1024)
-decodeSent = modifiedSentence.decode()
-myData = json.loads(decodeSent)
-
-for up in myData:
-   print(up)
-
-
-
-clientSocket.close()
+	clientSocket.close()
--- 03NetCentric/venv/src/serverTravel.py
+++ 03NetCentric/venv/src/serverTravel.py
@@ -14,6 +14,8 @@ serverSocket.listen(2)
 print('The server is ready to receive')
 while True:
 	connectionSocket, addr = serverSocket.accept()
+	print("Connected with " + addr[0] + ":" + str(addr[1]))
+	
 	sentence = connectionSocket.recv(1024).decode()
 	capitalizedSentence = commandHandler(sentence)
 	data = json.dumps(capitalizedSentence)
@@ -21,3 +23,4 @@ while True:
 	connectionSocket.send(data.encode())
 	connectionSocket.close()
 
+serverSocket.close()
